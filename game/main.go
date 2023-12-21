2b1d03cb1a2a3aef4f1a49d9e3a1affd5d6a7efc
Danil <danil-vinogradov-92@mail.ru>
2023-12-22 02:16:09 +0300

    Added structures

--- game/main.go
+++ game/main.go
@@ -5,6 +5,51 @@ package main
 	наверняка у вас будут какие-то структуры с методами, глобальные переменные ( тут можно ), функции
 */
 
+type World struct {
+	Rooms             []Room
+	Items             []Item
+	ExistingFurniture []Furniture
+}
+
+type Room struct {
+	Paths      []Room      //Комнаты в которые можно попасть
+	RFurniture []Furniture //Мебель находящаяся в комнате
+	Doors      []Door      //Двери в комнате
+}
+func (r Room) ShowPaths() []Room  {
+	return r.Paths
+}
+
+type Door struct {
+	State        string //Состояние двери (открыто/закрыто)
+	ItemToUnlock Item   //Предмет требующиеся для открытия
+}
+
+type Furniture struct {
+	Items []Item //Предмета находящиеся на/в мебели
+}
+
+type Item struct {
+	Name             string      //Название предмета
+	FurnitureToApply []Furniture //Мебель к которой можно применить объект
+	StorageItem      string      //Предмет требующийся для помещения объекта в инвентарь
+}
+
+type Command struct {
+	Title string //Название действия типо: "осмотреться"
+}
+
+func (Command)() {
+
+}
+
+type Player struct {
+	State     string //Состояние игрока
+	Inventory Item   //Поле для рюкзака
+	Location  Room   //Комната в которой сейчас находится игрок
+	Commands
+}
+
 func main() {
 	/*
 		в этой функции можно ничего не писать,
@@ -25,5 +70,6 @@ func handleCommand(command string) string {
 		данная функция принимает команду от "пользователя"
 		и наверняка вызывает какой-то другой метод или функцию у "мира" - списка комнат
 	*/
+
 	return "not implemented"
 }
